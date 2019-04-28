901eeb631df0155a4d37ca3086f93dbcc770c0df
indrakelabang <49917943+indrakelabang@users.noreply.github.com>
2019-04-28 15:46:49 +0700

    database wraper done
    koneksi dengan fetch assoc pd data base my sql

---
+++ phpmvc/app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//agar tidak usah menulis semua link ,cukup dijadikan BASEURL
+define ('BASEURL', 'http://localhost/phpmvc/public');
+
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
